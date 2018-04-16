94e4eae9a1a3b82fb433439cb22cbba8fa52a6bc
anusha <anushadevi.sunkara@gmail.com>
2018-04-17 00:21:45 +0530

    initial commit

---
+++ src/main/java/project1/anusha.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class anusha {
+
+	public String sample() {
+		return "this is deveops progam";
+		
+	}
+}
---
+++ src/test/java/project1/project1.java
@@ -0,0 +1,13 @@
+package project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class project1 {
+
+	@Test
+	public void test() {
+			}
+
+}
